e8c369c02392ba7b4d5c156c2a11570c399464f6
Your Name <you@example.com>
2024-03-06 21:50:53 +0530

    Project updated

--- index.php
+++ index.php
@@ -19,28 +19,26 @@
   <div class="preloader">
     <div class="spinner"></div>
   </div>
-  
+
   <div class="main-container">
     <div class="left-container">
-      <div class="left-inner-container">
-        <div class="logo">
-          <img src="./assets/images/Logo.svg" alt="logo" />
-        </div>
 
-        <div class="heading">
-          <h1 id="tagLines"></h1>
-          <span>Showcase your tech skills, or gain a few more.</span>
-        </div>
+      <div class="logo">
+        <img src="./assets/images/Logo.svg" alt="logo" />
       </div>
+
+      <div class="heading">
+        <h1 id="tagLines"></h1>
+        <span>Showcase your tech skills, or gain a few more.</span>
+      </div>
+
     </div>
     <div class="right-container">
-      <div class="right-inner-container">
-        <div class="links">
-          <h2>Get Started</h2>
-          <div class="btns">
-            <a href="./login.php">Login</a>
-            <a href="./register.php">Register</a>
-          </div>
+      <div class="links">
+        <h2>Get Started</h2>
+        <div class="btns">
+          <a href="./login.php">Login</a>
+          <a href="./register.php">Register</a>
         </div>
       </div>
     </div>
