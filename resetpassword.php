910ba579523d567860b402fa018f102d330e41cb
Your Name <you@example.com>
2024-03-14 12:29:13 +0530

    Project updated

--- resetpassword.php
+++ resetpassword.php
@@ -75,9 +75,7 @@ include "./assets/php/resetPasword.php";
       </div>
     </div>
   </div>
-
-  <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script type="module" src="./assets/js/login.js"></script> -->
+  <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
